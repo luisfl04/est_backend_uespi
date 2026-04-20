4d9bdffbfe28386d82e50c41e74680e50c39f738
luisfl04 <luisfelipecontato08@gmail.com>
2026-04-20 10:39:09 -0300

    feat: Adiciona refatoração na estrutura do código

--- login_desktop_to_api/ClientHttp.java
+++ login_desktop_to_api/ClientHttp.java
@@ -2,59 +2,104 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
 import java.net.URLEncoder;
 
 public class ClientHttp {
 
-    public String usuario, senha, url, urlParameters, resposta;
-    public int codretorno;
+    private String usuario, senha, url, urlParameters;
 
-
-    public ClientHttp(String us, String se, String ur) throws Exception {
-        this.setUrl(ur);
-        this.setLogin(us,se);
-    }
-    
-    public void setUrl(String ur) {
-        this.url = ur;
-    }
-
-    public void setLogin(String usu, String sen) throws Exception {
-        this.urlParameters = "usuario=" + URLEncoder.encode(usu, "UTF-8") + "&senha=" + URLEncoder.encode(sen, "UTF-8");
-    }
-
-    public String conecta() throws Exception {
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-        // Set request method
-        con.setRequestMethod("POST");
-
-        // Set headers (optional)
-        con.setRequestProperty("User-Agent", "Mozilla/5.0");
-        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-
-        // Send post request
-        con.setDoOutput(true);
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes(urlParameters);
-        wr.flush();
-        wr.close();
-
-        // Get return Code
-        int responseCode = con.getResponseCode();
-        this.codretorno = responseCode;
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+    public ClientHttp(String user, String senha, String url) throws Exception {
+        try {
+            this.setUrl(url);
+            this.setUsuario(user);
+            this.setSenha(senha);
+            this.setUrlParameters();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao construir o cliente HTTP: " + e.getMessage());
         }
-        in.close();
+    }
 
-        return response.toString();
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getCredentials() throws  Exception {
+        try {
+            return "usuario=" + URLEncoder.encode(this.getUsuario(), "UTF-8") + "&senha=" + URLEncoder.encode(this.getSenha(), "UTF-8");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getUrlParameters() {
+        return urlParameters;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setUrlParameters() throws Exception {
+        this.urlParameters = this.getCredentials();
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    private HttpURLConnection getUrlConnection() throws Exception{
+        try {
+            URI endpoint = new URI(this.getUrl());
+            return (HttpURLConnection) endpoint.toURL().openConnection();
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Erro ao estabelecer conexão com servidor: " + e.getMessage());
+        }
+    }
+
+    public String request() throws Exception {
+        try {
+            // Criando conexão com servidor
+            HttpURLConnection conexao = this.getUrlConnection();
+            conexao.setRequestProperty("User-Agent", "Mozilla/5.0");
+            conexao.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+            // Enviando requisição
+            conexao.setDoOutput(true);
+            DataOutputStream writerBody = new DataOutputStream(conexao.getOutputStream());
+            writerBody.writeBytes(this.getUrlParameters());
+            writerBody.flush();
+            writerBody.close();
+            int responseCode = conexao.getResponseCode();
+
+            // Manipulando resposta:
+            BufferedReader readerResponse = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = readerResponse.readLine()) != null) {
+                response.append(inputLine);
+            }
+            readerResponse.close();
+
+            return response.toString();
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Exception: " + e.getMessage());
+        }
+
     }
 }
--- login_desktop_to_api/LoginInterface.java
+++ login_desktop_to_api/LoginInterface.java
@@ -2,17 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.*;
-import javax.crypto.Cipher; import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
+import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
 
 public class LoginInterface extends JFrame{
-    private JTextField tlogin, tsenha, tretorno;
-    private JButton logar, limpar;
-    private JLabel llogin, lsenha, lretorno;
+    private JTextField inputLogin, inputSenha, inputRetorno;
+    private JButton buttonLogar, buttonLimpar;
     private static final String ALGORITHM = "AES";
 
     public LoginInterface() throws Exception {
@@ -21,63 +18,87 @@ public class LoginInterface extends JFrame{
         this.addComponentes();
     }
 
+    public JTextField getInputLogin() {
+        return inputLogin;
+    }
+
+    public JTextField getInputSenha() {
+        return inputSenha;
+    }
+
+    public JTextField getInputRetorno() {
+        return inputRetorno;
+    }
+
+    public void setInputLogin(JTextField inputLogin) {
+        this.inputLogin = inputLogin;
+    }
+
+    public void setInputSenha(JTextField inputSenha) {
+        this.inputSenha = inputSenha;
+    }
+
+    public void setInputRetorno(JTextField inputRetorno) {
+        this.inputRetorno = inputRetorno;
+    }
+
     public void addComponentes() throws Exception {
-        llogin = new JLabel("Login:");
-        add(llogin);
-        tlogin = new JTextField(20);
-        add(tlogin);
 
-        lsenha = new JLabel("Senha:");
-        add(lsenha);
-        tsenha = new JTextField(20);
-        add(tsenha);
+        JLabel labelLogin = new JLabel("Login:");
+        add(labelLogin);
+        this.setInputLogin(new JTextField(20));
+        add(this.getInputLogin());
 
-        lretorno = new JLabel("Resposta:");
-        add(lretorno);
-        tretorno = new JTextField(20);
-        add(tretorno);
+        JLabel labelSenha = new JLabel("Senha:");
+        add(labelSenha);
+        this.setInputSenha(new JTextField(20));
+        add(this.getInputSenha());
 
-        logar = new JButton("Logar");
+        JLabel labelRetorno = new JLabel("Resposta:");
+        add(labelRetorno);
+        this.setInputRetorno(new JTextField(20));
+        add(this.getInputRetorno());
+
+        this.buttonLogar = new JButton("Logar");
         logarOnClick();
         
-        limpar = new JButton("Limpar");
+        this.buttonLimpar = new JButton("Limpar");
         limparOnClick();
     }   
 
     public void logarOnClick() {
-            logar.addActionListener(new ActionListener() {
+            buttonLogar.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent evento) {
-                    if(evento.getSource() == logar) {
+                    if(evento.getSource() == buttonLogar) {
                         try {
                             String key = "1234567890123456";
-                            String encryptedString = encrypt(tsenha.getText(), key);
-                            ClientHttp Conexao = new ClientHttp(tlogin.getText(), encryptedString, "http://www.datse.com.br/dev/aulabackendv2.php");
-                            String ret = Conexao.conecta();
-                            tretorno.setText(ret);
+                            String encryptedString = encrypt(inputSenha.getText(), key);
+                            ClientHttp conexao = new ClientHttp(inputLogin.getText(), encryptedString, "https://www.datse.com.br/dev/aulabackendv2.php");
+                            String resposta = conexao.request();
+                            inputRetorno.setText(resposta);
                         } catch (Exception e) {
-                            System.out.println(e);
+                            System.out.println(e.getMessage());
                         }   
                     }
                 }
             }
         );
 
-        add(logar);
+        add(buttonLogar);
     }
 
-
     public void limparOnClick() {
-            limpar.addActionListener(new ActionListener() {
+            buttonLimpar.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent evento) {
-                    if(evento.getSource() == limpar){
-                        tlogin.setText("");
-                        tsenha.setText("");
-                        tretorno.setText("");
+                    if(evento.getSource() == buttonLimpar){
+                        inputLogin.setText("");
+                        inputSenha.setText("");
+                        inputRetorno.setText("");
                     }
                 }
             }
         );
-        add(limpar);
+        add(buttonLimpar);
     }
 
     public static String encrypt(String data, String key) throws Exception {
